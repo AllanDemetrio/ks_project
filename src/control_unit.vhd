1129a265cf95328b212cdfc0d21ad7d88f8d60e6
KarrLucas <77345091+KarrLucas@users.noreply.github.com>
2021-01-18 23:00:31 -0300

    Update control_unit.vhd
    Inicio da programação do control unit.

--- src/control_unit.vhd
+++ src/control_unit.vhd
@@ -1,19 +1,6 @@
 ----------------------------------------------------------------------------------
--- Company: UERGS
--- Engineer: Joao Leonardo Fragoso
--- 
--- Create Date:    19:08:01 06/26/2012 
--- Design Name:    K and S modeling
--- Module Name:    control_unit - rtl 
--- Description:    RTL Code for K and S control unit
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
---          0.02 - moving to Vivado 2017.3
--- Additional Comments: 
---
+-- Trabalho K&S
+-- Alunos: Allan Demetrio, João Carlos, Lucas Karr
 ----------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -45,20 +32,96 @@ entity control_unit is
 end control_unit;
 
 architecture rtl of control_unit is
--- signal added to test environment... remove this
-signal counter : std_logic_vector(7 downto 0);
+    type estados is (
+    FETCH,
+    DECODE,
+    PROX,
+    LOAD,
+    STORE,
+    MOVE,
+    ADD,
+    SUB,
+    ANDI,
+    ORI,
+    BRANCH,
+    BZERO,
+    BNEG,
+    NOP,
+    HALT
+    );
+    signal estado_atual : estados;
+    signal prox_estado : estados;
+
 begin
 
+    process (clk)
+        begin
+            if (clk'event and clk='1') then
+                if (rst_n='1') then
+                    estado_atual <= FETCH;
+                else
+                    estado_atual <= prox_estado;
+                end if;
+            end if;
+    end process;
+
+    process(clk)
+        begin
+            prox_estado <= estado_atual;
+            case(estado_atual) is
+                when FETCH =>
+                    ir_enable <= '1';
+                    flag_enable <= '0';
+                    pc_enable <='0';
+                    write_reg_enable <='0';
+                    prox_estado <= DECODE;
+
+                when DECODE =>
+                    case decoded_instruction is
+
+                        when I_NOP =>
+                            prox_estado <= NOP;
+
+                        when others =>
+                            prox_estado <= HALT;
+                            
+                    
+                    end case;
+                
+                when NOP =>
+                    ir_enable <= '0';
+                    flag_enable <= '0';
+                    pc_enable <='1';
+                    write_reg_enable <='0';
+                    prox_estado <= PROX;
+
+                when HALT =>
+                    ir_enable <= '0';
+                    flag_enable <= '0';
+                    pc_enable <='0';
+                    write_reg_enable <='0';
+                    halt <= '1';
+                    prox_estado <= HALT;                                  
+            
+                when others =>  --PROX
+                    ir_enable <= '0';
+                    flag_enable <= '0';
+                    pc_enable <='1';
+                    write_reg_enable <='0';
+                    prox_estado <= FETCH;
+
+            end case ;
+    end process ;
+
 --process to test environment ... remove this
-    main: process(clk, rst_n)
-    begin
-        if (rst_n = '0') then
-            counter <= (others => '0');
-        elsif (clk'event and clk='1') then
-            counter <= counter + 1;
-        end if;
-    end process main;
-    halt <= '1' when counter = x"5f" else '0';
+--    main: process(clk, rst_n)
+--    begin
+--        if (rst_n = '0') then
+--            counter <= (others => '0');
+--        elsif (clk'event and clk='1') then
+--            counter <= counter + 1;
+--        end if;
+--    end process main;
+--    halt <= '1' when counter = x"5f" else '0';
 -- remove until here....
 end rtl;
-
